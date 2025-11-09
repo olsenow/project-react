cea61a4a26b5e9e174c35c73498e9d5316a40292
olsenow <olsenow4263@outlook.com>
2025-11-10 03:22:53 +0800

    Initial commit

---
+++ src/Homepage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+export default function Homepage() {
+	return (
+		<main className="bg-sky-600 text-white h-screen flex flex-col ">
+			<div className="m-14 gap-4 mb-8 justify-center items-center">
+				<h1 className="text-9xl antialiased text-shadow-lg/70 text-shadow-black-600 leading-[1.5] text-left " >Ow Xun Jiun</h1>
+				<hr className="border-2 border-white w-[700px] my-4"/>
+			</div>
+			<div className="m-14 gap-4 mb-8 justify-center items-center">
+				<p className="text-5xl antialiased text-shadow-lg/70 text-shadow-black-600 text-center leading-[3] text-left">IoT Developer</p>
+			</div>
+			<div className="">
+				<p className="text-3xl text-pretty max-w-3xl text-justify text-shadow-lg/70 text-shadow-600">I'm an IoT fresh graduate who builds embedded systems with sensors and microcontrollers with a solid understanding and basic software concept knowledge.</p>
+				
+			</div>
+		</main>
+	);
+}
+
